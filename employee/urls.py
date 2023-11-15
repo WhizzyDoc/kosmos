a2fe43b4224612d5611bd4d22a0a55d3714d6811
Yusuf Saubana Mayowa <101975553+Crane04@users.noreply.github.com>
2023-11-15 07:53:35 +0100

    Update urls.py

--- employee/urls.py
+++ employee/urls.py
@@ -6,7 +6,6 @@ router = routers.DefaultRouter()
 #router.register('users', views.ProfileViewSet)
 
 urlpatterns = [
-    # path('', include(router.urls)),
     path("login", views.LoginView.as_view(), name = "login"),
     path("profile/<str:api_token>", views.ProfileView.as_view(), name = "profile"),
     path("bank/<str:api_token>", views.BankAccountCreateView.as_view(), name = "bank"),
@@ -21,5 +20,10 @@ urlpatterns = [
     path("groups/<str:api_token>", views.GroupChats.as_view(), name = "groups"),
     path("group/<str:api_token>/<str:id>", views.GroupChatDetailsView.as_view(), name = "group-chat"),
     path("chat/<api_token>/<str:pk>", views.ChatMessageCreateView.as_view(), name = "chat"),
-    path("query/<str:api_token>", views.QueryView.as_view(), name = "query")
+    path("query/<str:api_token>", views.QueryView.as_view(), name = "query"),
+    path("activities/<str:api_token>", views.LogView.as_view(), name = "activities"),
+    path("notifications/<str:api_token>", views.NotificationsView.as_view(), name = "notifications"),
+    path("forgotpassword_getnew", views.ForgotPassword_GetNewPasswordView.as_view(), name = "forgotpassword-getnew"),
+    path("forgotpassword_reset", views.ResetPassword.as_view(), name = "reset-password"),
+
 ]
